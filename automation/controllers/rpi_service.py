3bab435f91daeb35fc38f8c6fc38f51d5c7de7a5
Phil Stahlfeld <philstahlfeld@gmail.com>
2013-12-06 03:28:07 +0000

    whoops. forgot this

---
+++ automation/controllers/rpi_service.py
@@ -0,0 +1,15 @@
+import atexit
+from depot.automation.controllers import service
+
+
+class GPIOService(service.Service):
+
+  def __init__(self, *args, **kwargs):
+    from RPi import GPIO as gpio
+    self.gpio = gpio
+    super(GPIOService, self).__init__(*args, **kwargs)
+
+  @atexit.register
+  def Cleanup():
+    import RPi.GPIO as gpio
+    gpio.cleanup()
