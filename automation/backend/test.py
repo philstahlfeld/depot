6d0e4e5750230dca04d1b31d61790cfe6f264d8d
Phil Stahlfeld <philstahlfeld@gmail.com>
2013-12-06 22:46:28 -0500

    Totally functional (well mostly)

--- automation/backend/test.py
+++ automation/backend/test.py
@@ -1,4 +1,5 @@
 from depot.automation.communication import action_message
+from depot.automation.communication import message
 from depot.automation.communication import services_message
 from depot.automation.communication import status_message
 from depot.automation.controllers import board
@@ -9,7 +10,8 @@ import socket
 if __name__ == '__main__':
   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   s.connect(('10.1.10.18', 14025))
-  msg = action_message.ActionMessage(service_name='Bedroom',
-                                      action=services.OutletService.Toggle)
+  msg = services_message.ServicesMessage()
   msg.SendOverSocket(s)
+  msg = message.ReceiveOverSocket(s)
+  print msg.services
   s.close()
--- automation/frontend/flask/server.py
+++ automation/frontend/flask/server.py
@@ -3,9 +3,17 @@ import pdb
 import socket
 
 from depot.automation.communication import message
+from depot.automation.communication import action_message
 from depot.automation.communication import services_message
+from depot.automation.communication import status_message
+from depot.automation.controllers import services
 from depot.media.music.pandora.pianobar import radio_info
 from depot.media.music.pandora.pianobar import remote
+
+
+action_mapper = {
+    services.SWITCHABLE.name: services.SWITCHABLE,
+}
 
 app = flask.Flask(__name__)
 
@@ -17,6 +25,7 @@ def Index():
   render_data['radio_buttons'] = GetPandoraControlBar()
 
   render_data['services_path'] = flask.url_for('Services')
+  render_data['services_action_url'] = flask.url_for('ServicesControl')
   return flask.render_template('base.html', **render_data)
 
 def GetPandoraControlBar():
@@ -28,15 +37,50 @@ def GetPandoraControlBar():
   radio_buttons.append({'name': 'Restart', 'action': 'restart'})
   return radio_buttons
 
-@app.route('/services')
-def Services():
+def GetSocket():
   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   s.connect(('10.1.10.18', 14025))
+  return s
+
+@app.route('/services')
+def Services():
+  s = GetSocket()
   msg = services_message.ServicesMessage()
   msg.SendOverSocket(s)
   msg = message.ReceiveOverSocket(s)
   s.close()
-  return flask.render_template('services.html', service_dict=msg.services)
+  ds_list = []
+  for name in msg.services:
+    msg2 = status_message.StatusMessage(name)
+    s = GetSocket()
+    msg2.SendOverSocket(s)
+    msg2 = message.ReceiveOverSocket(s)
+    s.close()
+    ds_list.append(DisplayService(name, msg.services[name].name, msg2.status))
+  return flask.render_template('services.html', services=ds_list)
+
+
+
+class DisplayService(object):
+
+  def __init__(self, name, flavor, status):
+    self.name = name
+    self.flavor = flavor
+    self.status = status
+
+@app.route('/service_control', methods=['POST'])
+def ServicesControl():
+  data = flask.request.form
+  action_type = action_mapper[data['flavor']]
+
+  if action_type == services.SWITCHABLE:
+    msg = action_message.ActionMessage(service_name=data['service_name'],
+                                       action=services.OutletService.Toggle)
+    s = GetSocket()
+    msg.SendOverSocket(s)
+    s.close()
+
+  return 'Success'
 
 @app.route('/radio')
 def Radio():
@@ -83,4 +127,4 @@ def Login():
 
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=True)
+  app.run(host='0.0.0.0', port=80)
