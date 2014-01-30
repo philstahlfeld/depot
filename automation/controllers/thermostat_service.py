5ca32e306066ca27b4fa4ecc60562aa463f00ef7
pes014 <pes014@bucknell.edu>
2014-01-29 18:35:16 -0800

    This service is totally worth $3.2B

---
+++ automation/controllers/thermostat_service.py
@@ -0,0 +1,53 @@
+import threading
+import time
+
+from depot.automation.controllers import service
+
+
+THERMOSTAT = service.ServiceFlavor('thermostat')
+
+
+class Thermostat(service.Service):
+  """Wraps an outlet service to control a heater."""
+
+  def __init__(self, name, outlet, temperature=72):
+    super(Thermostat, self).__init__(name=name, flavor=THERMOSTAT)
+    self._outlet = outlet
+    self._target = temperature
+    self._stop = True
+    self.Start()
+
+  def Start(self):
+    self._stop = False
+    t = threading.Thread(target=self._ControlOutlet)
+    t.start()
+
+  def Stop(self):
+    self._stop = True
+
+  def GetTemperature(self):
+    # TODO: return float representing temp in F (from Adafruit code)
+    return 71
+
+  def Status(self):
+    outlet = self._outlet.Status()
+    temp = self.GetTemperature()
+    return (temp, self._target, outlet)
+
+  def _ControlOutlet(self):
+    while True:
+      self._DoControl()
+      if self._stop:
+        return
+      time.sleep(30)
+
+  def _DoControl(self):
+    """Keep temperature in a 5 degree window."""
+    temp = self.GetTemperature()
+    if temp > self._target + 2.5:
+      self._outlet.TurnOff()
+    elif temp < self._target - 2.5:
+      self._outlet.TurnOn()
+
+
+
