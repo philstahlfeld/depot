ee2c3d949838ca7f5699a080b52b1668a479711a
pes014 <pes014@bucknell.edu>
2014-04-26 11:27:59 -0700

    Added remote manager

---
+++ automation/frontend/service_manager.py
@@ -0,0 +1,64 @@
+import socket
+import threading
+
+from depot.automation.communication import message
+from depot.automation.communication import services_message
+from depot.automation.communication import status_message
+
+
+class RemoteManager(object):
+
+  def __init__(self, name, remote_ip, remote_port):
+    self._ip = remote_ip
+    self._port = remote_port
+    self._services = {}  # Map service name to (flavor, status)
+    self._update = True  # Keep thread going
+    self._name = name
+
+  def _OpenSocket(self):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((self._ip, self._port))
+    return s
+
+  def Start(self):
+    self._update = True
+    t = threading.Thread(target=self._RunUpdater)
+    t.daemon = True
+    t.start()
+
+  def Stop(self):
+    self._update = False
+
+  def _RunUpdater(self):
+    while self._update:
+      print 'updating'
+      self._Update()
+      time.sleep(5)
+
+  # Needs to be synchonized
+  def _Update(self):
+    s = self._OpenSocket()
+    msg = services_message.ServicesMessage()
+    msg.SendOverSocket(s)
+    msg = message.ReceiveOverSocket(s)
+    s.close()
+    for name in msg.services:
+      s = self._OpenSocket()
+      msg2 = status_message.StatusMessage(name)
+      msg2.SendOverSocket(s)
+      msg2 = message.ReceiveOverSocket(s)
+      s.close()
+      self._services[name] = (msg.services[name], msg2.status)
+
+  def __repr__(self):
+    rtn = '%s\n' % self._name
+    for name in self._services:
+      flavor = self._services[name][0].name
+      status = self._services[name][1]
+      rtn += '  %s => (%s, %s)\n' % (name, flavor, status)
+    return rtn
+
+if __name__ == '__main__':
+  mgr = RemoteManager('Birdhouse', '10.1.10.17', 14025)
+  mgr.Start()
+  print mgr
